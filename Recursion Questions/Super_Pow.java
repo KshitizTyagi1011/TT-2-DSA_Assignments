8c49e8d1b5af7e7a1a10d210632eb7cb802d0259
kshitiz Tyagi <kshitiztyagi1011@gmail.com>
2023-06-16 21:06:50 +0530

    16-06-2023

---
+++ Recursion Questions/Super_Pow.java
@@ -0,0 +1,27 @@
+// 372. Super Pow
+class Solution {
+    int base = 1337;
+
+    public int superPow(int a, int[] b) {
+        return superPow(a, b, b.length-1);
+    }
+
+    public int superPow(int a, int[] b, int index) {
+        if (index == 0)
+            return pow(a, b[index]) % base;
+        int pre = superPow(a, b, index-1);
+        return (pow(pre, 10) * pow(a, b[index])) % base;
+    }
+
+    public int pow(int x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x % base;
+        int half = pow(x, n/2); 
+        if(n%2==0)  
+            return (half*half) % base;
+        else  
+            return (((half*half) % base) *(x % base)) % base; 
+    }
+}
